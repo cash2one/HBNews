44a31ab53af2d2407c25f5666a6f003399c57586
huawuzui <389317202@qq.com>
2015-11-23 15:12:48 +0800

    del

--- common/libxl-3.1.0_破解/libxl-3.1.0/include_c/handle.h
+++
@@ -1,9 +0,0 @@
-#ifndef HANDLE_H
-#define HANDLE_H
-
-typedef struct tagBookHandle * BookHandle;
-typedef struct tagSheetHandle * SheetHandle;
-typedef struct tagFormatHandle * FormatHandle;
-typedef struct tagFontHandle * FontHandle;
-
-#endif
--- common/protobuf/c#/build.bat
+++
@@ -1,8 +0,0 @@
-protogen -i:as_c_proto.proto -o:as_c_proto.cs
-protogen -i:gs_c_proto.proto -o:gs_c_proto.cs
-pause
-protogen -i:gs_c_register_or_login.proto -o:gs_c_register_or_login.cs
-protogen -i:gs_c_msg_body.proto -o:gs_c_msg_body.cs
-protogen -i:tnt_deploy_goods_info.proto -o:tnt_deploy_goods_info.cs
-protogen -i:bag_item.proto -o:bag_item.cs
-pause
